07b3b1119f82cbda6747b80850a0552b7a3685fe
KhamichenkaIrina <irinakhamichenka@mail.ru>
2023-09-17 22:40:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return Number(
+        String(n)
+            .split("")
+            .filter((item) => item !== "-")
+            .reverse()
+            .join("")
+    );
+};
